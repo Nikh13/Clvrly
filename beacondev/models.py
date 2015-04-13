8bd88b4a8825a784ebd4fe2bf47f4f2ed6da3732
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2015-04-13 07:24:16 +0530

    Change db to ndb(supports automatic caching) and Event model

--- beacondev/models.py
+++ beacondev/models.py
@@ -1,24 +1,37 @@
-from google.appengine.ext import db
+from google.apipengine.ext import ndb
+from google.appengine.ext.ndb import msgprop
+from protorpc import messages
 
 
-class Beacon(db.Model):
-    nickname = db.StringProperty()
-    beaconuuid = db.StringProperty()
-    groupids = db.ListProperty(int)
-    description = db.TextProperty()
+class Beacon(ndb.Model):
+    nickname = ndb.StringProperty()
+    beaconuuid = ndb.StringProperty()
+    groupids = ndb.ListProperty(int)
+    description = ndb.TextProperty()
 
 
-class Group(db.Model):
-    nickname = db.StringProperty()
-    triggerids = db.ListProperty(int)
-    description = db.TextProperty()
+class Group(ndb.Model):
+    nickname = ndb.StringProperty()
+    triggerids = ndb.ListProperty(int)
+    description = ndb.TextProperty()
 
 
-class Trigger(db.Model):
-    nickname = db.StringProperty()
+class Trigger(ndb.Model):
+    nickname = ndb.StringProperty()
     # 0 for image, 1 for video, 2 for advert, 3 for coupon
-    linktype = db.IntegerProperty()
-    description = db.StringProperty()
-    triggerlink = db.StringProperty()
+    linktype = ndb.IntegerProperty()
+    description = ndb.StringProperty()
+    triggerlink = ndb.StringProperty()
     # 0 for immediate, 1 for near and 2 for far
-    triggerwhen = db.IntegerProperty()
+    triggerwhen = ndb.IntegerProperty()
+
+
+class Buildings(messages.Enum):
+    TT = 'Technology Tower'
+    SJT = 'Silver Jubilee Tower'
+
+
+class Event(ndb.Model):
+    name = ndb.StringProperty()
+    time = ndb.DateTimeProperty()
+    building = msgprop.EnumProperty(Buildings, required=True)
